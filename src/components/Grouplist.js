8afee994a91c0ebe785dabd69a0a7d041ac5eb02
MesbahArrafat <mesbaharrafat.22@gmail.com>
2023-01-25 01:36:19 +0600

    create group/Group request send/User group list show

--- src/components/Grouplist.js
+++ src/components/Grouplist.js
@@ -1,98 +1,101 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import {BsThreeDotsVertical} from "react-icons/bs";
+import { getDatabase, ref, onValue, set, push, remove} from "firebase/database";
+import { useSelector } from 'react-redux';
 
 const Grouplist = () => {
+    const db = getDatabase();
+    let data = useSelector((state) => state.userLoginInfo.userInfo);
+    let [show,setShow] = useState(false);
+    let [gname,setGname] = useState("");
+    let [gtag,setGtag] = useState("");
+    let [grouplist,setGrouplist] = useState([]);
+
+
+    let handleGroupButton = ()=>{
+      setShow(!show);
+    };
+    
+    let handleGroupCreate = ()=>{
+        set(push(ref(db, 'group')), {
+            groupname: gname,
+            grouptagline: gtag,
+            adminid: data.uid,
+            adminname: data.displayName,
+           }).then(()=>{
+            setShow(false);
+           });
+    };
+
+    useEffect(()=>{
+        const groupRef = ref(db, 'group');
+        onValue(groupRef, (snapshot) => {
+            let arr =[];
+            snapshot.forEach((item)=>{
+             if(data.uid != item.val().adminid){
+                arr.push({...item.val(), key: item.key});
+             }
+            
+            });
+            setGrouplist(arr);
+        });
+    },[]);
+
+    let handleGroupJoin = (item)=>{
+        set(push(ref(db, 'groupJoinRequest')), {
+            groupid:item.key,
+            groupname: item.groupname,
+            grouptagline: item.grouptagline,
+            adminid: item.adminid,
+            adminname: item.adminname,
+            userid:data.uid,
+            username:data.displayName,
+           })
+    };
   return (
     <div className="w-full bg-white shadow-lg rounded-lg py-3 px-5 mt-7
     h-[305px] overflow-scroll">
      <div className="relative">
        <h3 className="font-nunito font-bold text-xl">Group List</h3>
-        <BsThreeDotsVertical className='absolute top-[6px] right-[30px] text-primary'/>
+
+       <button onClick={handleGroupButton}className="font-nunito font-bold text-xl bg-primary  py-1 px-2 rounded absolute top-[0px] right-[30px] text-white">
+        {show ? "Go Back" : "Create  Group"}</button>
      </div>
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
+     {show 
+     ?(
+        <div className='mt-8'>
+          <input type='text' className='border border-solid border-x-secondary w-full py-3 px-3 mb-3 outline-0 rounded'
+          placeholder='Group Name'
+          onChange={(e)=>setGname(e.target.value)}/>
+
+          <input type='text' className='border border-solid border-x-secondary w-full py-3 px-3 mb-3 outline-0 rounded'
+          placeholder='Group Tagline'
+          onChange={(e)=>setGtag(e.target.value)}/>
+
+          <button onClick={handleGroupCreate} className='w-full bg-primary py-2.5 rounded font-nunito font-semibold text-xl text-white mt-3' >Create</button>
+        </div>
+      
+     ) 
+     :
+     (grouplist.map(item=>(
+        <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
         <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/grouplist.png'/>
+            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
         </div>
         <div>
-        <h3 className="font-nunito font-bold text-xl">Friends Reunion</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys, Wassup!</p>
+        <h3 className="font-nunito font-bold text-xl">{item.groupname}</h3>
+        <p className="font-nunito font-bold text-[14px] text-[#4D4D4D]">Admin: {item.adminname}</p>
+        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">{item.grouptagline}</p>
         </div>
         <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Join</button>
+            <button
+            onClick={() => handleGroupJoin(item)} className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Join</button>
         </div>
-     </div>
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/grouplist.png'/>
         </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Friends Reunion</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Join</button>
-        </div>
-     </div>
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/grouplist.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Friends Reunion</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Join</button>
-        </div>
-     </div>
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/grouplist.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Friends Reunion</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Join</button>
-        </div>
-     </div>
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/grouplist.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Friends Reunion</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Join</button>
-        </div>
-     </div>
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/grouplist.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Friends Reunion</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Join</button>
-        </div>
-     </div>
-     <div className='flex gap-x-5 items-center pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/grouplist.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Friends Reunion</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys, Wassup!</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Join</button>
-        </div>
-     </div>
+       )))
+      }
+     
+
     </div>
   )
 }
--- src/components/Mygroups.js
+++ src/components/Mygroups.js
@@ -1,123 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {BsThreeDotsVertical} from "react-icons/bs";
+import { getDatabase, ref, onValue} from "firebase/database";
+import { useSelector } from 'react-redux';
+
 
 const Mygroups = () => {
+    const db = getDatabase();
+    let data = useSelector((state) => state.userLoginInfo.userInfo);
+    let [grouplist,setGrouplist] = useState([]);
+    let [grouprequestlist,setGrouprequestlist] = useState([]);
+    let [show,setShow] = useState(false);
+    
+    useEffect(()=>{
+        const groupRef = ref(db, 'group');
+        onValue(groupRef, (snapshot) => {
+            let arr =[];
+            snapshot.forEach((item)=>{
+             if(data.uid == item.val().adminid){
+                arr.push({...item.val(), key: item.key});
+             }
+            
+            });
+            setGrouplist(arr);
+        });
+    },[]);
+
+    let handleGroupRequestShow = (gitem) => {
+        setShow(true);
+        const groupRef = ref(db, 'groupJoinRequest');
+        onValue(groupRef, (snapshot) => {
+            let arr =[];
+            snapshot.forEach((item)=>{
+             if(data.uid == item.val().adminid && item.val().groupid == gitem.key){
+                arr.push({...item.val(), key: item.key});
+             }
+            
+            });
+            setGrouprequestlist(arr);
+        });   
+    };
+
   return (
     <div className="w-full bg-white shadow-lg rounded-lg py-3 px-5 mt-3.5
     h-[335px] overflow-scroll">
      <div className="relative">
        <h3 className="font-nunito font-bold text-xl">My Groups</h3>
-        <BsThreeDotsVertical className='absolute top-[6px] right-[30px] text-primary'/>
+       {show && 
+       (<button onClick={()=>setShow(false)}className="font-nunito font-bold text-xl bg-primary  py-1 px-2 rounded absolute top-[0px] right-[30px] text-white">Go Back</button>
+       )}
      </div>
-
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Charry Lofez</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Accept</button>
-        </div>
-     </div>
-
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Charry Lofez</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Accept</button>
-        </div>
-     </div>
-
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Charry Lofez</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Accept</button>
-        </div>
-     </div>
-
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Charry Lofez</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Accept</button>
-        </div>
-     </div>
-
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Charry Lofez</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Accept</button>
-        </div>
-     </div>
-
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Charry Lofez</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Accept</button>
-        </div>
-     </div>
-
-     <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Charry Lofez</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Accept</button>
-        </div>
-     </div>
-
-     <div className='flex gap-x-5 items-center  pb-3.5 mt-3.5'>
-        <div>
-            <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
-        </div>
-        <div>
-        <h3 className="font-nunito font-bold text-xl">Charry Lofez</h3>
-        <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">Hi Guys</p>
-        </div>
-        <div>
-            <button className='font-nunito font-bold text-xl bg-primary text-white py-2.5 px-5 rounded'>Accept</button>
-        </div>
-     </div>
-
-
-
-    </div>
-  )
-}
+     {grouplist.length == 0
+     ?
+     (<p className='bg-red-500 p-2.5 text-white'>No Group Available</p>)
+    :
+    (
+        show 
+        ? 
+        grouprequestlist.map(item=>(
+            <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
+            <div>
+                <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
+            </div>
+            <div>
+            <h3 className="font-nunito font-bold text-xl">{item.username}{""}</h3>
+            </div>
+            <div>
+                <button className='font-nunito font-bold  bg-primary text-white p-1.5 rounded mr-1'>Accept</button>
+                <button className='font-nunito font-bold  bg-red-500 text-white p-1.5 rounded'>Reject</button>
+            </div>
+            </div>
+        ))
+        :
+        grouplist.map(item=>(
+            <div className='flex gap-x-5 items-center border-b border-solid border-primary pb-3.5 mt-3.5'>
+            <div>
+                <img className='w-[70ox] h-[70px] rounded-full' src='images/friendrequest.png'/>
+            </div>
+            <div>
+            <h3 className="font-nunito font-bold text-xl">{item.groupname}</h3>
+            <p className="font-nunito font-bold text-[14px] text-[#4D4D4D]">Admin: {item.adminname}</p>
+            <p className="font-nunito font-semibold text-[14px] text-[#4D4D4D]">{item.grouptagline}</p>
+            </div>
+            <div>
+                <button className='font-nunito font-bold  bg-primary text-white p-1.5 rounded mb-2'>Info</button><br/>
+                <button onClick={()=>handleGroupRequestShow(item)} className='font-nunito font-bold  bg-primary text-white p-1.5 rounded'>Request</button>
+            </div>
+            </div>
+            ))
+    )}
+     
+   </div>
+  );
+};
 
 export default Mygroups
